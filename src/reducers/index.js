96b323ce14ef70703f10cc2bbe60e657ddad2e40
erik-sandoval <erik.sandoval@icloud.com>
2019-05-21 22:36:11 -0700

    almost have delete working.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import { loginReducer } from "./loginReducer";
 import { registerReducer } from "./registerReducer";
 import { getJokesReducer } from "./getJokesReducer";
 import { getWalletReducer } from "./getWalletReducer";
+import { delJokeReducer } from "./delJokeReducer";
 
 const rootReducer = combineReducers({
   login: loginReducer,
   register: registerReducer,
   getJokes: getJokesReducer,
-  getWallet: getWalletReducer
+  getWallet: getWalletReducer,
+  delJoke: delJokeReducer
 });
 
 export default rootReducer;

30cc8ca0d4e3f53797b4809d685e7fd1af6d5d6a
Erik Sandoval <erik.sandoval@icloud.com>
2019-10-18 15:51:10 -0700

    refactored to hooks.

--- src/components/AddJokeForm.js
+++ src/components/AddJokeForm.js
@@ -1,63 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Form, Input, Button, Icon } from "semantic-ui-react";
 
 import { addJoke } from "../actions";
 
-class AddJokeForm extends React.Component {
-    state = {
-        setup: "",
-        punch_line: ""
-    };
+const AddJokeForm = props => {
+    const [setup, setSetup] = useState("");
+    const [punch_line, setPunch] = useState("");
 
-    handleChange = e => {
-        this.setState({
-            ...this.state,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
-        this.props
-            .addJoke(this.state)
-            .then(res => this.props.history.push("/jokesfeed"));
+        props
+            .addJoke({ setup, punch_line })
+            .then(res => props.history.push("/jokesfeed"));
     };
-    render() {
-        return (
-            <div>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group widths='equal'>
-                        <Form.Field>
-                            <label>Setup</label>
-                            <Input
-                                fluid
-                                name={"setup"}
-                                onChange={this.handleChange}
-                                value={this.state.setup}
-                            />
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Punch Line</label>
-                            <Input
-                                fluid
-                                name={"punch_line"}
-                                onChange={this.handleChange}
-                                value={this.state.punch_line}
-                            />
-                        </Form.Field>
-                    </Form.Group>
-                    <Button animated>
-                        <Button.Content visible>Next</Button.Content>
-                        <Button.Content hidden>
-                            <Icon name='arrow right' />
-                        </Button.Content>
-                    </Button>
-                </Form>
-            </div>
-        );
-    }
-}
+
+    console.log(props);
+    return (
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group widths='equal'>
+                    <Form.Field>
+                        <label>Setup</label>
+                        <Input
+                            fluid
+                            name={"setup"}
+                            onChange={e => setSetup(e.target.value)}
+                            value={setup}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Punch Line</label>
+                        <Input
+                            fluid
+                            name={"punch_line"}
+                            onChange={e => setPunch(e.target.value)}
+                            value={punch_line}
+                        />
+                    </Form.Field>
+                </Form.Group>
+                <Button animated>
+                    <Button.Content visible>Next</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name='arrow right' />
+                    </Button.Content>
+                </Button>
+            </Form>
+        </div>
+    );
+};
 
 export default connect(
     null,
